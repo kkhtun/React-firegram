f4ce935e1fa5248734be564143be7dc5d7738bd7
Khaing Khant Htun <70341036+kkhtun@users.noreply.github.com>
2021-10-06 10:31:01 +0630

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,7 @@ import "firebase/storage";
 import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyCdrtsu84kiFFI8Ncy9WB8CD9XAZQUzy1o",
-  authDomain: "react-firegram-b4dd0.firebaseapp.com",
-  projectId: "react-firegram-b4dd0",
-  storageBucket: "react-firegram-b4dd0.appspot.com",
-  messagingSenderId: "398800348784",
-  appId: "1:398800348784:web:9e7cc79f705124473f36b5",
-  measurementId: "G-B6VGEJ636Z",
-};
+var firebaseConfig = {} // Put your config obj here
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
